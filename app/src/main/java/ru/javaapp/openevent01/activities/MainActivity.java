b2bf636d6d7f7f3c41d8ef71b7cdfef9047ef637
ildar2244 <ildar2244@gmail.com>
2015-06-29 12:12:56 +0300

    recyclerView

--- app/src/main/java/ru/javaapp/openevent01/activities/MainActivity.java
+++ app/src/main/java/ru/javaapp/openevent01/activities/MainActivity.java
@@ -3,17 +3,27 @@ package ru.javaapp.openevent01.activities;
 import android.content.res.Configuration;
 import android.os.AsyncTask;
 import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Toast;
+import android.support.design.widget.NavigationView;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
@@ -36,9 +46,10 @@ import java.util.Map;
 import ru.javaapp.openevent01.R;
 import ru.javaapp.openevent01.adapters.EventsAdapter;
 import ru.javaapp.openevent01.dao.AllEvents;
+import ru.javaapp.openevent01.fragments.EventListFragment;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     private ListView lv_events;
 
@@ -48,36 +59,48 @@ public class MainActivity extends ActionBarActivity {
     private ActionBarDrawerToggle mDrawerToggle;
     private String mActivityTitle;
 
-    private String jsonResult;
-    private String url = "http://javaapp.ru/select_events_from_Events_table.php";
+    //private String jsonResult;
+    //private String url = "http://javaapp.ru/select_events_from_Events_table.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        lv_events = (ListView)findViewById(R.id.lv_events);
-        mDrawerList = (ListView)findViewById(R.id.navList);
+        //lv_events = (ListView)findViewById(R.id.lv_events);
+        //mDrawerList = (ListView)findViewById(R.id.navList);
         mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
         mActivityTitle = getTitle().toString();
 
-        addDrawerItems();
-        setupDrawer();
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
+        final ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+        ab.setDisplayHomeAsUpEnabled(true);
 
-        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(MainActivity.this, "Time for an upgrade!", Toast.LENGTH_SHORT).show();
-            }
-        });
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        if (navigationView != null) {
+            setupDrawerContent(navigationView);
+        }
 
-       new JsonReadTask().execute();
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        if (viewPager == null) {
+            setupViewPager(viewPager);
+        }
+
+        //addDrawerItems();
+        //setupDrawer();
+
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //getSupportActionBar().setHomeButtonEnabled(true);
+
+
+
+       //new JsonReadTask().execute();
     }
 
     // Async Task to access the web
-    private class JsonReadTask extends AsyncTask<String, Void, String> {
+    /*private class JsonReadTask extends AsyncTask<String, Void, String> {
         @Override
         protected String doInBackground(String... params) {
             HttpClient httpclient = new DefaultHttpClient();
@@ -120,18 +143,12 @@ public class MainActivity extends ActionBarActivity {
             ListDrwaer();
         }
 
-    }// end async task
+    }*/
 
-    /*
-    public void accessWebService() {
-        JsonReadTask task = new JsonReadTask();
-        // passes values for the urls string array
-        task.execute(new String[] { url });
-    }
-    */
+
 
     // build hash set for list view
-    public void ListDrwaer() {
+    /*public void ListDrwaer() {
         AllEvents event;
         ArrayList<AllEvents> eventsList = new ArrayList<AllEvents>();
 
@@ -161,27 +178,27 @@ public class MainActivity extends ActionBarActivity {
 
         EventsAdapter eventsAdapter = new EventsAdapter(MainActivity.this, eventsList);
         lv_events.setAdapter(eventsAdapter);
-    }
+    }*/
 
 
-    private void addDrawerItems() {
+    /*private void addDrawerItems() {
         String[] osArray = { "Ночная жизнь", "Театры", "Спорт", "Хуйня какая-то", "Херня" };
         mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
         mDrawerList.setAdapter(mAdapter);
 
-    }
+    }*/
 
-    private void setupDrawer() {
+    /*private void setupDrawer() {
         mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
 
-            /** Called when a drawer has settled in a completely open state. */
+
             public void onDrawerOpened(View drawerView) {
                 super.onDrawerOpened(drawerView);
                 getSupportActionBar().setTitle("Категории!");
                 invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
             }
 
-            /** Called when a drawer has settled in a completely closed state. */
+
             public void onDrawerClosed(View view) {
                 super.onDrawerClosed(view);
                 getSupportActionBar().setTitle(mActivityTitle);
@@ -191,20 +208,7 @@ public class MainActivity extends ActionBarActivity {
 
         mDrawerToggle.setDrawerIndicatorEnabled(true);
         mDrawerLayout.setDrawerListener(mDrawerToggle);
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        // Sync the toggle state after onRestoreInstanceState has occurred.
-        mDrawerToggle.syncState();
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        mDrawerToggle.onConfigurationChanged(newConfig);
-    }
+    }*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -218,12 +222,91 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        // Activate the navigation drawer toggle
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
+        switch (item.getItemId()) {
+
+            case android.R.id.home:
+            {
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            }
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        Adapter adapter = new Adapter(getSupportFragmentManager());
+        adapter.addFragment(new EventListFragment(), "Category 1");
+        //adapter.addFragment(new CheeseListFragment(), "Category 2");
+        //adapter.addFragment(new CheeseListFragment(), "Category 3");
+        viewPager.setAdapter(adapter);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+                        Adapter adapter = new Adapter(getSupportFragmentManager());
+
+                        switch (menuItem.getItemId()) {
+                            case R.id.nav_home: {
+                                adapter.addFragment(new EventListFragment(), "Category 1");
+                                viewPager.setAdapter(adapter);
+                                menuItem.setChecked(true);
+                                mDrawerLayout.closeDrawers();
+                                return true;
+                            }
+                            case R.id.nav_messages: {
+                                adapter.addFragment(new EventListFragment(), "Category 2");
+                                viewPager.setAdapter(adapter);
+                                menuItem.setChecked(true);
+                                mDrawerLayout.closeDrawers();
+                                return true;
+                            }
+                            case R.id.nav_friends: {
+                                adapter.addFragment(new EventListFragment(), "Category 3");
+                                viewPager.setAdapter(adapter);
+                                menuItem.setChecked(true);
+                                mDrawerLayout.closeDrawers();
+                                return true;
+                            }
+                        }
+
+                /*menuItem.setChecked(true);
+                mDrawerLayout.closeDrawers();*/
+                        return true;
+                    }
+                });
+    }
+
+    static class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragments = new ArrayList<>();
+        private final List<String> mFragmentTitles = new ArrayList<>();
+
+        public Adapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragments.add(fragment);
+            mFragmentTitles.add(title);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitles.get(position);
+        }
     }
 }
